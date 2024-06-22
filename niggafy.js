e7a47a3eb16b78962acaa1c4bf9d7699745e0d23
offensiveevil <157976369+offensiveevil@users.noreply.github.com>
2024-06-22 08:51:56 -0700

    Deleted . . .

--- niggafy.js
+++
@@ -1,7 +0,0 @@
-const mainElement = document.querySelector(".main-container");
-const niggafyButton = document.querySelector("#niggafy-action");
-
-niggafyButton.addEventListener("click", (target) => {
-  mainElement.style.backgroundColor = "black";
-  niggafyButton.style.visibility = "hidden";
-});
